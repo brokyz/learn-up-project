aaccb5e313bd036ce8d064e64f7d470efac1be4a
boqiang <boqiang6450@163.com>
2024-02-17 14:34:53 +0800

    更改测试数据

--- src/views/tree/tree.vue
+++ src/views/tree/tree.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="views-main">
     <div class="tree-main">
-      <o-tree 
+      <o-tree
         :tree-options="treeData"
         :is-accordion="false"
         type="default"
@@ -9,7 +9,8 @@
         @expand="treeExpand"
         v-model="treeValue"
         v-model:checked-keys="checkedKeys"
-        v-model:expanded-keys="expandedKeys">
+        v-model:expanded-keys="expandedKeys"
+      >
       </o-tree>
     </div>
   </div>
@@ -21,43 +22,71 @@ import { ref } from 'vue'
 
 const treeValue = ref(13)
 const checkedKeys = ref([13])
-const expandedKeys = ref([1,7])
+const expandedKeys = ref([1, 7])
 // 创建数据
 const treeData = ref<Array<TreeOption>>([
-  { key: 1, label: '选项1', children: [
-    { key: 7, label: '选项2', children: [
-      { key: 13, label: '选项2' },
-      { key: 14, label: '选项2' },
-      { key: 15, label: '选项2' },
-    ]  },
-    { key: 8, label: '选项2'},
-    { key: 9, label: '选项2', children: [
-      { key: 16, label: '选项2' },
-      { key: 17, label: '选项2' },
-      { key: 18, label: '选项2' },
-    ] },
-  ] },
-  { key: 2, label: '选项2', children: [
-    { key: 10, label: '选项2' },
-    { key: 11, label: '选项2' },
-    { key: 12, label: '选项2', children: [
-      { key: 19, label: '选项2', children: [
-        { key: 20, label: '选项2' },
-        { key: 21, label: '选项2' },
-      ] },
-    ] },
-  ]  },
-  { key: 3, label: '选项3' },
-  { key: 4, label: '选项4' },
-  { key: 5, label: '选项5' },
-  { key: 6, label: '选项6', children: [
-    { key: 22, label: '选项2' },
-    { key: 23, label: '选项2' },
-    { key: 24, label: '选项2' },
-    { key: 25, label: '选项2' },
-    { key: 26, label: '选项2' },
-    { key: 27, label: '选项2' },
-  ] },
+  {
+    key: 1,
+    label: '首项1',
+    children: [
+      {
+        key: 7,
+        label: '次项1',
+        children: [
+          { key: 13, label: '次次项1' },
+          { key: 14, label: '次次项2' },
+          { key: 15, label: '次次项3' },
+        ],
+      },
+      { key: 8, label: '次项2' },
+      {
+        key: 9,
+        label: '次项3',
+        children: [
+          { key: 16, label: '次次项1' },
+          { key: 17, label: '次次项2' },
+          { key: 18, label: '次次项3' },
+        ],
+      },
+    ],
+  },
+  {
+    key: 2,
+    label: '首项1',
+    children: [
+      { key: 10, label: '次项1' },
+      { key: 11, label: '次项2' },
+      {
+        key: 12,
+        label: '次项3',
+        children: [
+          {
+            key: 19,
+            label: '次次项1',
+            children: [
+              { key: 20, label: '次次次项1' },
+              { key: 21, label: '次次次项2' },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  { key: 3, label: '首项1' },
+  { key: 4, label: '首项1' },
+  { key: 5, label: '首项1' },
+  {
+    key: 6,
+    label: '首项1',
+    children: [
+      { key: 22, label: '次项1' },
+      { key: 23, label: '次项2' },
+      { key: 24, label: '次项3' },
+      { key: 25, label: '次项4' },
+      { key: 26, label: '次项5' },
+      { key: 27, label: '次项6' },
+    ],
+  },
 ])
 
 const clickItem = (item: any) => {
@@ -68,7 +97,6 @@ const clickItem = (item: any) => {
 const treeExpand = (value: Array<number>) => {
   console.log(value)
 }
-
 </script>
 
 <style scoped lang="less">
